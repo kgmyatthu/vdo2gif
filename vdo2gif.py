4088bd242dafcaf9f9e2ca012d571e06f3958b77
kaung-myat-thu <2962000kaungmyatthu@gmail.com>
2020-04-04 13:51:13 -0400

    commit on master

--- vdo2gif.py
+++ vdo2gif.py
@@ -6,7 +6,8 @@ argparser = argparse.ArgumentParser()
 
 argparser.add_argument('inputdir',type=str, help='Input directory for video file')
 argparser.add_argument('outputdir',type=str,help='Output directory for gif file')
-argparser.add_argument('-t', nargs=2, action='store', dest='time', metavar=('0:0:0','0:0:0'), help='the video within the timeline will be converted gif when specify.(Format: hr:min:sec)')
+argparser.add_argument('-f','--fps',type=int, action='store',dest='framerate',default=25, help='framerate (less frame, less size of gif). default:25')
+argparser.add_argument('-t', '--time',nargs=2, action='store', dest='time', metavar=('0:0:0','0:0:0'), help='the video within the timeline will be converted gif when specify.(Format: hr:min:sec)')
 
 args = argparser.parse_args()
 
@@ -14,7 +15,7 @@ startTime = list()
 endTime = list()
 args.outputdir = args.outputdir.replace(".gif", "")
 banner = """
-created by -
+scripted by -
      _   __                       ___  ___            _ _____ _           
     | | / /                       |  \/  |           | |_   _| |          
     | |/ /  __ _ _   _ _ __   __ _| .  . |_   _  __ _| |_| | | |__  _   _ 
@@ -34,12 +35,12 @@ if args.time != None:
     if len(startTime) > 3 or len(endTime) > 3:
         argparser.print_help()
         sys.exit()
-    clip = (VideoFileClip(args.inputdir).subclip((float(startTime[0]),float(startTime[1]),float(startTime[2])),(float(endTime[0]),float(endTime[1]),float(endTime[2]))))
+    clip = (VideoFileClip(args.inputdir).subclip((float(startTime[0]),float(startTime[1]),float(startTime[2])),(float(endTime[0]),float(endTime[1]),float(endTime[2]))).resize(0.8))
     
 if args.time == None:
     clip = VideoFileClip(args.inputdir)
 
 print(banner)
-clip.write_gif(args.outputdir+'.gif')
+print(args.framerate)
+clip.write_gif(args.outputdir+'.gif',fps=args.framerate,program='ffmpeg')
   
-
