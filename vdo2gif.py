0d88b008554ed21b2ec144b5125ac7bef356d390
comandpromt <vayase_a_ferrol@hotmail.com>
2021-08-22 19:53:08 +0200

    v 2.0

--- vdo2gif.py
+++ vdo2gif.py
@@ -1,22 +1,33 @@
 #!bin/python3
+
 import argparse
+
 from moviepy.editor import *
 
 argparser = argparse.ArgumentParser()
 
 argparser.add_argument('inputdir',type=str, help='Input directory for video file')
+
 argparser.add_argument('outputdir',type=str,help='Output directory for gif file')
+
 argparser.add_argument('-r','--resize',type=float,action='store',dest='resize',default=1,help='scale the gif dimension relative to original video file.(0-1) default:1')
+
 argparser.add_argument('-s','--speed',type=float,action='store',dest='speed',default=1,help='modify the speed of the gif relative to original video. default:1')
+
 argparser.add_argument('-f','--fps',type=int, action='store',dest='framerate',default=25, help='framerate. default:25')
+
 argparser.add_argument('-t', '--time',nargs=2, action='store', dest='time', metavar=('0:0:0','0:0:0'), help='the video within the timeline will be converted gif when specify.(Format: hr:min:sec)')
 
 args = argparser.parse_args()
 
 startTime = list()
+
 endTime = list()
+
 args.outputdir = args.outputdir.replace(".gif", "")
+
 banner = """
+
 scripted by -
      _   __                       ___  ___            _ _____ _           
     | | / /                       |  \/  |           | |_   _| |          
